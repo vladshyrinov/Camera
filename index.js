a5c8563ec16499075625b3c01cd954d9ab962684
Vlad Shyrinov <vlad.shyrinov@gmail.com>
2018-01-27 15:03:09 +0200

    camera

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var app = express();
-var port = process.env.PORT || 3000;
-
-app.get("/", function(req, res) {
-    res.send("Welcome to nodejs app on Heroku");
-});
-
-app.listen(port);
