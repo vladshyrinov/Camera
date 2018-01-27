a4a4dbc51610e43bd85ee486db92733ddb2ab937
Vlad Shyrinov <vlad.shyrinov@gmail.com>
2018-01-27 18:00:03 +0200

    png

--- scripts/app.js
+++ scripts/app.js
@@ -15,6 +15,6 @@ if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 // Trigger photo take
 document.getElementById("snap").addEventListener("click", function() {
     context.drawImage(video, 0, 0, 640, 480);
-    var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    var image = canvas.toDataURL("image/png");
     window.location.href = image;
 });
